f0822227a2f7ada7e95dfdc8eb63f61a8f3f401e
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-10-09 18:57:55 -0300

    feat: loader for computer scene feat. mediaQUery for the canva

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styles } from "../styles";
-import{ComputersCanvas} from "./canvas"
+import { ComputersCanvas } from "./canvas"
+import { motion } from "framer-motion"
 
 
 const Hero = () => {
@@ -13,10 +14,29 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>Hi! I'm <span className='text-[#915eff]'>Rafaela</span></h1>
-        <p className={`${styles.heroSubText} mt-2`}> I develop web applications <br className='sm:block hidden' /></p>
+          <p className={`${styles.heroSubText} mt-2`}> I develop web applications <br className='sm:block hidden' /></p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
+        <a href='#about'>
+          <div className='w-[35px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev
+              animate={{
+                y: [0, 16, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,53 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import Loader from '../Loader';
 
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  console.log(isMobile);
 
-const Computers = () => {
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<Loader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -25,30 +68,12 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
 }
-const ComputersCanvas = () => {
-  return (
-    <Canvas
-      frameloop='demand'
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<Loader/>}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
-      </Suspense>
-      <Preload all />
-    </Canvas>
-  )
-}
+
 export default ComputersCanvas;
